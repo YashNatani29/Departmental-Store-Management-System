056c7f6726757adf16969ebae9b0e385ea67bda3
YashNatani29 <74348190+YashNatani29@users.noreply.github.com>
2023-01-21 04:27:20 +0530

    ibii

---
+++ src/departmentalstore/JLabel.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package departmentalstore;
+
+/**
+ *
+ * @author Admin
+ */
+class JLabel {
+
+    JLabel(String string) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+}
